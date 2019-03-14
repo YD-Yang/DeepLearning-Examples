fd6e55d849e325cb80c6274e42fb1926b5058d18
YD-Yang <48411346+YD-Yang@users.noreply.github.com>
2019-03-13 23:34:14 -0500

    Update and rename wtte-rnn2.py to engine_failure.py

--- RNN_Churn/engine_failure.py
+++ RNN_Churn/engine_failure.py
@@ -174,27 +174,3 @@ plt.scatter(test_predict[:, 0], test_predict[:, 1])
 plt.xlabel('predicted churn date')
 plt.ylabel('true churn date')
 
-
-################################################################################
-
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.models import model_from_json
-# serialize model to JSON
-model_json = model.to_json()
-with open("model.json", "w") as json_file:
-    json_file.write(model_json)
-# serialize weights to HDF5
-model.save_weights("model.h5")
-print("Saved model to disk")
-
-
-
-# load json and create model
-json_file = open('model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-loaded_model = model_from_json(loaded_model_json)
-# load weights into new model
-loaded_model.load_weights("model.h5")
-print("Loaded model from disk")
